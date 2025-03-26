ad0f2923dd0ec5dc22dde24deef59e89599db7bf
Kate-Gornostaeva <itsmekate153@gmail.com>
2025-03-26 18:42:15 +1000

    daily_out

--- SmthNew/main/urls.py
+++ SmthNew/main/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('new', views.new, name='about'),
-    path('daily', views.daily, name='daily'),
+    #path('daily', views.daily, name='daily'),
     path('contact', views.contacts, name='contacts'),
 ]
